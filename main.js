293903e30544cdf0f8f5682da8999dca276538db
AmithaMadhav <amithamadhavcs@gmail.com>
2024-10-17 17:27:12 +0530

    task10 v1

---
+++ main.js
@@ -0,0 +1,74 @@
+let currMoleTile;
+let currPlantTile;
+let mark = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    for (let i = 0; i < 9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
+        document.getElementById("play").appendChild(tile);
+    }
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000); 
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+    if (currMoleTile) {
+        currMoleTile.innerHTML = "";
+    }
+    let mole = document.createElement("img");
+    mole.src = "./images/piranha-plant.png";
+
+    let num = getRandomTile();
+    if (currPlantTile && currPlantTile.id == num) {
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currPlantTile) {
+        currPlantTile.innerHTML = "";
+    }
+    let plant = document.createElement("img");
+    plant.src = "./images/mario.png";
+
+    let num = getRandomTile();
+    if (currMoleTile && currMoleTile.id == num) {
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    if (this == currMoleTile) {
+        mark += 10;
+        document.getElementById("mark").innerText = mark.toString();
+    }
+    else if (this == currPlantTile) {
+        document.getElementById("mark").innerText = mark.toString()+"\nGAME OVER"; 
+        gameOver = true;
+    }
+}
